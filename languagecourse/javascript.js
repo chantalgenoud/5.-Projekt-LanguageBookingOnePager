d58b67900ff6f9bd56630467157ec55efc87cd99
chantalgenoud <chantal.genoud@gmail.com>
2022-03-02 11:18:08 +0100

    day 3d

---
+++ languagecourse/javascript.js
@@ -0,0 +1,10 @@
+/*
+
+ function validateForm() {
+        let vorname = document.getElementById("vorname").value; 
+
+        sessionStorage.setItem("vorname", vorname); //wert speichern im Memory des Browsers
+        alert("vorname = "+vorname);
+    }
+
+        //document.getElementById("vorname").innerHTML; // das nimmt den wert aus dem cache und setzte sie als id = vorname Die Eigenschaft Element.innerHTML liest und speichert den Inhalt eines HTML-Elements. Wenn Sie beim dynamischen Ändern des gespeicherten Inhalts HTML-Tags notieren, werden diese bei der Aktualisierung des Elementinhalts interpretiert. */
