35156c6a30b7f4363950a8c576d45f7af63a722e
chantalgenoud <chantal.genoud@gmail.com>
2022-03-02 14:54:03 +0100

    java 2

--- languagecourse/javascript.js
+++ languagecourse/javascript.js
@@ -4,7 +4,7 @@ function onSubmit() {
     get value from selected element
     assign value to new variable calles firstname*/
 
-    let firstName = document.getElementById("vorname").value; /* also weist den wert VORNAME der Variable firstName zu*/
+    let firstName = document.getElementById("firstName").value; /* also weist den wert VORNAME der Variable firstName zu*/
 
     /*falls keinen wert negierung = !*/ /*mit Javascript besser, da mehr möglichkeiten und es auf allen Browsern funktioniert*/
     if (!firstName) {
@@ -13,14 +13,16 @@ function onSubmit() {
     } 
 
     sessionStorage.setItem("firstName", document.getElementById("firstName").value) // key name erstes speichert als das ab (set), das zweite ist der key value
-
+}
 
 
  function validateForm() {
-        let vorname = document.getElementById("vorname").value; 
+        let vorname = document.getElementById("firstName").value; 
 
-        sessionStorage.setItem("vorname", vorname); //wert speichern im Memory des Browsers
-        alert("vorname = "+vorname);
+        sessionStorage.setItem("firstName", firstName); //wert speichern im Memory des Browsers
+        alert("firstName= "+firstName);
     }
 
-        //document.getElementById("vorname").innerHTML; // das nimmt den wert aus dem cache und setzte sie als id = vorname Die Eigenschaft Element.innerHTML liest und speichert den Inhalt eines HTML-Elements. Wenn Sie beim dynamischen Ändern des gespeicherten Inhalts HTML-Tags notieren, werden diese bei der Aktualisierung des Elementinhalts interpretiert. */
+
+
+        //document.getElementById("firstName").innerHTML; // das nimmt den wert aus dem cache und setzte sie als id = vorname Die Eigenschaft Element.innerHTML liest und speichert den Inhalt eines HTML-Elements. Wenn Sie beim dynamischen Ändern des gespeicherten Inhalts HTML-Tags notieren, werden diese bei der Aktualisierung des Elementinhalts interpretiert. */
